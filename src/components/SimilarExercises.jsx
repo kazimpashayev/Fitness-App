505a3dd96a23f7c55f3a31fcbce7c4c5efd8abd2
kazimpashayev <kazimpasayev@protonmail.com>
2023-12-07 00:08:38 +0400

    Added some styles

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -5,7 +5,13 @@ import Loader from './Loader';
 function SimilarExercises({ targetMuscleExercises, equipmentExercises }) {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '0' } }}>
-      <Typography variant="h4" mt={5} ml={5} mb={5}>
+      <Typography
+        variant="h5"
+        mt={5}
+        ml={5}
+        mb={5}
+        sx={{ fontSize: { xs: '30px' }, ml: { xs: '30px' } }}
+      >
         Exercises that target the same muscle group
       </Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
@@ -15,7 +21,13 @@ function SimilarExercises({ targetMuscleExercises, equipmentExercises }) {
           <Loader />
         )}
       </Stack>
-      <Typography variant="h4" mt={5} ml={5} mb={5}>
+      <Typography
+        variant="h4"
+        mt={5}
+        ml={5}
+        mb={5}
+        sx={{ fontSize: { xs: '30px' }, ml: { xs: '30px' } }}
+      >
         Exercises that use the same equipment
       </Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
