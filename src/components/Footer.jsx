5460282c6cebc248d432a1186dff3182ac77265f
kazimpashayev <kazimpasayev@protonmail.com>
2023-12-07 00:07:04 +0400

    Added some styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,12 @@ function Footer() {
     <Box mt="80px" bgcolor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="the logo" width="200px" height="40px" />
-        <Typography variant="h5" pb="40px" mt="20px">
+        <Typography
+          variant="h5"
+          pb="40px"
+          mt="20px"
+          sx={{ fontSize: { xs: '20px' } }}
+        >
           Made with ❤️ by Kazim Pashayev
         </Typography>
       </Stack>
