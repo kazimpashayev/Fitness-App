e3318bf518c08651c7cc4ed38be1c64e5a346f51
kazimpashayev <kazimpasayev@protonmail.com>
2025-01-15 20:11:09 +0400

    Added minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,12 @@
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/Logo.png';
 
 function Footer() {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
-      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="the logo" width="200px" height="40px" />
-        <Typography
-          variant="h5"
-          pb="40px"
-          mt="20px"
-          sx={{ fontSize: { xs: '20px' } }}
-        >
+      <Stack gap="30px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="the logo" />
+        <Typography variant="h5" pb="20px" sx={{ fontSize: { xs: '20px' } }}>
           Made with ❤️ by Kazim Pashayev
         </Typography>
       </Stack>
